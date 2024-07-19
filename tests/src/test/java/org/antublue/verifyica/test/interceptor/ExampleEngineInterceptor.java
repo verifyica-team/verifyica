33cc19ca0a7a1fe13de0bf2f1a4cd2f886a70235
antublue <antublue@antublue.org>
2024-07-18 23:33:21 -0400

    Code cleanup
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/src/test/java/org/antublue/verifyica/test/interceptor/ExampleEngineInterceptor.java
+++ tests/src/test/java/org/antublue/verifyica/test/interceptor/ExampleEngineInterceptor.java
@@ -48,6 +48,9 @@ public class ExampleEngineInterceptor implements EngineInterceptor {
                     engineInterceptorContext.getEngineContext().getStore().remove(KEY);
                     break;
                 }
+            default: {
+                // DO NOTHING
+            }
         }
     }
 }
