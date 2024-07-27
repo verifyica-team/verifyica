b2281318cf8bf29a29ba6e862044e68d665ad8d9
antublue <antublue@antublue.org>
2024-07-27 12:53:58 -0400

    Updated test
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/src/test/java/org/antublue/verifyica/test/argument/NoArgumentsTest.java
+++ tests/src/test/java/org/antublue/verifyica/test/argument/NoArgumentsTest.java
@@ -16,10 +16,8 @@
 
 package org.antublue.verifyica.test.argument;
 
-import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.antublue.verifyica.api.ArgumentContext;
+import org.antublue.verifyica.api.ClassContext;
 import org.antublue.verifyica.api.Verifyica;
 
 /** Example test */
@@ -30,33 +28,38 @@ public class NoArgumentsTest {
         return null;
     }
 
-    @Verifyica.Test
-    @Verifyica.Order(order = 1)
-    public void test1(ArgumentContext argumentContext) throws Throwable {
-        System.out.println(format("test1(%s)", argumentContext.getTestArgument()));
+    @Verifyica.Prepare
+    public static void prepare(ClassContext classContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
+    }
 
-        assertThat(argumentContext).isNotNull();
-        assertThat(argumentContext.getStore()).isNotNull();
-        assertThat(argumentContext.getTestArgument()).isNotNull();
+    @Verifyica.BeforeAll
+    public void beforeAll(ArgumentContext argumentContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
+    }
+
+    @Verifyica.BeforeEach
+    public void beforeEach(ArgumentContext argumentContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
     }
 
     @Verifyica.Test
-    @Verifyica.Order(order = 0)
-    public void test2(ArgumentContext argumentContext) throws Throwable {
-        System.out.println(format("test2(%s)", argumentContext.getTestArgument()));
-
-        assertThat(argumentContext).isNotNull();
-        assertThat(argumentContext.getStore()).isNotNull();
-        assertThat(argumentContext.getTestArgument()).isNotNull();
+    public void test(ArgumentContext argumentContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
     }
 
     @Verifyica.Test
-    @Verifyica.Order(order = 0)
-    public void test3(ArgumentContext argumentContext) throws Throwable {
-        System.out.println(format("test3(%s)", argumentContext.getTestArgument()));
+    public void afterEach(ArgumentContext argumentContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
+    }
 
-        assertThat(argumentContext).isNotNull();
-        assertThat(argumentContext.getStore()).isNotNull();
-        assertThat(argumentContext.getTestArgument()).isNotNull();
+    @Verifyica.Test
+    public void afterAll(ArgumentContext argumentContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
+    }
+
+    @Verifyica.Conclude
+    public static void conclude(ClassContext classContext) throws Throwable {
+        throw new IllegalStateException("Should not be executed");
     }
 }
