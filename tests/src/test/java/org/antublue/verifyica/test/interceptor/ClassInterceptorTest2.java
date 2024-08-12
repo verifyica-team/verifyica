d41f1acf627562c2566a6fd0c8484b819b1fb953
antublue <antublue@antublue.org>
2024-08-11 22:27:12 -0400

    Updated tests
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/src/test/java/org/antublue/verifyica/test/interceptor/ClassInterceptorTest2.java
+++ tests/src/test/java/org/antublue/verifyica/test/interceptor/ClassInterceptorTest2.java
@@ -16,31 +16,38 @@
 
 package org.antublue.verifyica.test.interceptor;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import org.antublue.verifyica.api.ArgumentContext;
 import org.antublue.verifyica.api.ClassContext;
 import org.antublue.verifyica.api.Verifyica;
-import org.antublue.verifyica.api.interceptor.ArgumentInterceptorContext;
 import org.antublue.verifyica.api.interceptor.ClassInterceptor;
-import org.antublue.verifyica.api.interceptor.ClassInterceptorContext;
-import org.antublue.verifyica.api.interceptor.engine.EngineInterceptorContext;
-import org.antublue.verifyica.engine.interceptor.ClassInterceptorRegistry;
 
 /** Example test */
 public class ClassInterceptorTest2 {
 
-    static {
-        // Directly register a ClassInterceptor
-        ClassInterceptorRegistry.getInstance()
-                .register(ClassInterceptorTest2.class, new ConcreteClasInterceptor());
+    @Verifyica.ClassInterceptorSupplier
+    public static ClassInterceptor[] interceptors1() {
+        System.out.printf("%s interceptors1()%n", ClassInterceptorTest2.class.getName());
+
+        ClassInterceptor[] classInterceptors = new ClassInterceptor[2];
+        classInterceptors[0] = new ExampleClassInterceptor1();
+        classInterceptors[1] = new ExampleClassInterceptor1();
+
+        return classInterceptors;
     }
 
-    // Antipattern, but used for testing
-    public static List<String> actual = new ArrayList<>();
+    @Verifyica.ClassInterceptorSupplier
+    public static Collection<ClassInterceptor> interceptors2() {
+        System.out.printf("%s interceptors2()%n", ClassInterceptorTest2.class.getName());
+
+        Collection<ClassInterceptor> collection = new ArrayList<>();
+
+        collection.add(new ExampleClassInterceptor1());
+        collection.add(new ExampleClassInterceptor2());
+
+        return collection;
+    }
 
     @Verifyica.ArgumentSupplier
     public static String arguments() {
@@ -50,292 +57,48 @@ public class ClassInterceptorTest2 {
     @Verifyica.Prepare
     public static void prepare(ClassContext classContext) throws Throwable {
         System.out.printf("  %s prepare()%n", classContext.getTestClass().getName());
-
-        actual.add("prepare");
     }
 
     @Verifyica.BeforeAll
     public void beforeAll(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s beforeAll()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("beforeAll");
     }
 
     @Verifyica.BeforeEach
     public void beforeEach(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s beforeEach()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("beforeEach");
     }
 
     @Verifyica.Test
     @Verifyica.Order(order = 0)
     public void test1(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s test1()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("test1");
     }
 
     @Verifyica.Test
     @Verifyica.Order(order = 1)
     public void test2(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s test2()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("test2");
     }
 
     @Verifyica.Test
     @Verifyica.Order(order = 2)
     public void test3(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s test3()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("test3");
     }
 
     @Verifyica.AfterEach
     public void afterEach(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s afterEach()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("afterEach");
     }
 
     @Verifyica.AfterAll
     public void afterAll(ArgumentContext argumentContext) throws Throwable {
         System.out.printf("  %s afterAll()%n", argumentContext.getTestArgument().getPayload());
-
-        actual.add("afterAll");
     }
 
     @Verifyica.Conclude
     public static void conclude(ClassContext classContext) throws Throwable {
         System.out.printf("  %s conclude()%n", classContext.getTestClass().getName());
-
-        actual.add("conclude");
-    }
-
-    public static class ConcreteClasInterceptor implements ClassInterceptor {
-
-        @Override
-        public void preInstantiate(
-                EngineInterceptorContext engineInterceptorContext, Class<?> testClass)
-                throws Throwable {
-            System.out.printf("%s preInstantiate()%n", getClass().getName());
-
-            actual.add("preInstantiate");
-        }
-
-        @Override
-        public void postInstantiate(
-                EngineInterceptorContext engineInterceptorContext,
-                Class<?> testClass,
-                Object testInstance,
-                Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postInstantiate()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postInstantiate");
-        }
-
-        @Override
-        public void prePrepare(ClassInterceptorContext classInterceptorContext) throws Throwable {
-            System.out.printf("%s prePrepare()%n", getClass().getName());
-
-            actual.add("prePrepare");
-        }
-
-        @Override
-        public void postPrepare(
-                ClassInterceptorContext classInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postPrepare()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postPrepare");
-        }
-
-        @Override
-        public void preBeforeAll(ArgumentInterceptorContext argumentInterceptorContext)
-                throws Throwable {
-            System.out.printf("%s preBeforeAll()%n", getClass().getName());
-
-            actual.add("preBeforeAll");
-        }
-
-        @Override
-        public void postBeforeAll(
-                ArgumentInterceptorContext argumentInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postBeforeAll()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postBeforeAll");
-        }
-
-        @Override
-        public void preBeforeEach(ArgumentInterceptorContext argumentInterceptorContext)
-                throws Throwable {
-            System.out.printf("%s preBeforeEach()%n", getClass().getName());
-
-            actual.add("preBeforeEach");
-        }
-
-        @Override
-        public void postBeforeEach(
-                ArgumentInterceptorContext argumentInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postBeforeEach()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postBeforeEach");
-        }
-
-        @Override
-        public void preTest(
-                ArgumentInterceptorContext argumentInterceptorContext, Method testMethod)
-                throws Throwable {
-            System.out.printf("%s preTest()%n", getClass().getName());
-
-            actual.add("preTest");
-        }
-
-        @Override
-        public void postTest(
-                ArgumentInterceptorContext argumentInterceptorContext,
-                Method testMethod,
-                Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postTest()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postTest");
-        }
-
-        @Override
-        public void preAfterEach(ArgumentInterceptorContext argumentInterceptorContext)
-                throws Throwable {
-            System.out.printf("%s preAfterEach()%n", getClass().getName());
-
-            actual.add("preAfterEach");
-        }
-
-        @Override
-        public void postAfterEach(
-                ArgumentInterceptorContext argumentInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postAfterEach()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postAfterEach");
-        }
-
-        @Override
-        public void preAfterAll(ArgumentInterceptorContext argumentInterceptorContext)
-                throws Throwable {
-            System.out.printf("%s preAfterAll()%n", getClass().getName());
-
-            actual.add("preAfterAll");
-        }
-
-        @Override
-        public void postAfterAll(
-                ArgumentInterceptorContext argumentInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postAfterAll()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postAfterAll");
-        }
-
-        @Override
-        public void preConclude(ClassInterceptorContext classInterceptorContext) throws Throwable {
-            System.out.printf("%s preConclude()%n", getClass().getName());
-
-            actual.add("preConclude");
-        }
-
-        @Override
-        public void postConclude(
-                ClassInterceptorContext classInterceptorContext, Throwable throwable)
-                throws Throwable {
-            System.out.printf("%s postConclude()%n", getClass().getName());
-
-            assertThat(throwable).isNull();
-            actual.add("postConclude");
-        }
-
-        @Override
-        public void onDestroy(ClassInterceptorContext classInterceptorContext) throws Throwable {
-            System.out.printf("%s onDestroy()%n", getClass().getName());
-
-            List<String> expected = new ArrayList<>();
-
-            expected.add("preInstantiate");
-            expected.add("postInstantiate");
-
-            String state = "prepare";
-            expected.add("pre" + capitalize(state));
-            expected.add(state);
-            expected.add("post" + capitalize(state));
-
-            state = "beforeAll";
-            expected.add("pre" + capitalize(state));
-            expected.add(state);
-            expected.add("post" + capitalize(state));
-
-            for (int i = 1; i <= 3; i++) {
-                state = "beforeEach";
-                expected.add("pre" + capitalize(state));
-                expected.add(state);
-                expected.add("post" + capitalize(state));
-
-                state = "test";
-                expected.add("pre" + capitalize(state));
-                expected.add(state + i);
-                expected.add("post" + capitalize(state));
-
-                state = "afterEach";
-                expected.add("pre" + capitalize(state));
-                expected.add(state);
-                expected.add("post" + capitalize(state));
-            }
-
-            state = "afterAll";
-            expected.add("pre" + capitalize(state));
-            expected.add(state);
-            expected.add("post" + capitalize(state));
-
-            state = "conclude";
-            expected.add("pre" + capitalize(state));
-            expected.add(state);
-            expected.add("post" + capitalize(state));
-
-            assertThat(actual.size()).isEqualTo(expected.size());
-
-            int pad = pad(expected);
-
-            for (int i = 0; i < expected.size(); i++) {
-                System.out.printf(
-                        "expected [%-" + pad + "s] actual [%-" + pad + "s]%n",
-                        expected.get(i),
-                        actual.get(i));
-
-                assertThat(actual.get(i)).isEqualTo(expected.get(i));
-            }
-        }
-    }
-
-    private static String capitalize(String string) {
-        return Character.toUpperCase(string.charAt(0)) + string.substring(1);
-    }
-
-    private static int pad(List<String> strings) {
-        int pad = 0;
-
-        for (String string : strings) {
-            pad = Math.max(string.length(), pad);
-        }
-
-        return pad;
     }
 }
