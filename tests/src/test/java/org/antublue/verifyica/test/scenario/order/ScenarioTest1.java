7a35d2532b4d51e83df684715d70a30ea29aa96a
antublue <antublue@antublue.org>
2024-09-22 20:41:07 -0400

    Updated test
    Signed-off-by: antublue <antublue@antublue.org>

--- tests/src/test/java/org/antublue/verifyica/test/scenario/order/ScenarioTest1.java
+++ tests/src/test/java/org/antublue/verifyica/test/scenario/order/ScenarioTest1.java
@@ -34,20 +34,6 @@ public class ScenarioTest1 {
                 argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
     }
 
-    @Verifyica.Test(order = 2)
-    public void step2(ArgumentContext argumentContext) throws Throwable {
-        System.out.printf(
-                "step2(name[%s], payload[%s])%n",
-                argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
-    }
-
-    @Verifyica.Test(order = 3)
-    public void step4(ArgumentContext argumentContext) throws Throwable {
-        System.out.printf(
-                "step4(name[%s], payload[%s])%n",
-                argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
-    }
-
     @Verifyica.Test(order = 4)
     public void step1(ArgumentContext argumentContext) throws Throwable {
         System.out.printf(
@@ -55,10 +41,24 @@ public class ScenarioTest1 {
                 argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
     }
 
+    @Verifyica.Test(order = 2)
+    public void step2(ArgumentContext argumentContext) throws Throwable {
+        System.out.printf(
+                "step2(name[%s], payload[%s])%n",
+                argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
+    }
+
     @Verifyica.Test(order = 5)
     public void step3(ArgumentContext argumentContext) throws Throwable {
         System.out.printf(
                 "step3(name[%s], payload[%s])%n",
+                argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
+    }
+
+    @Verifyica.Test(order = 3)
+    public void step4(ArgumentContext argumentContext) throws Throwable {
+        System.out.printf(
+                "step4(name[%s], payload[%s])%n",
                 argumentContext.getTestArgument(), argumentContext.getTestArgument().getPayload());
     }
 
