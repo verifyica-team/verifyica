206d7147913e5bbebdb741d244798a6019961155
antublue <antublue@antublue.org>
2024-10-21 01:09:13 -0400

    Added TemporaryDirectory
    Signed-off-by: antublue <antublue@antublue.org>

---
+++ api/src/main/java/org/verifyica/api/TemporaryDirectory.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2024-present Verifyica project authors and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.verifyica.api;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.UUID;
+
+/** Class to implement TemporaryDirectory */
+public class TemporaryDirectory implements AutoCloseable {
+
+    private final Path temporaryDirectory;
+
+    /**
+     * Constructor
+     *
+     * @throws IOException IOException if the temporary directory can't be created
+     */
+    public TemporaryDirectory() throws IOException {
+        this("temp-");
+    }
+
+    /**
+     * Constructor
+     *
+     * @param prefix prefix
+     * @throws IOException IOException if the temporary directory can't be created
+     */
+    public TemporaryDirectory(String prefix) throws IOException {
+        if (prefix == null) {
+            throw new IllegalArgumentException("prefix is null");
+        }
+
+        if (prefix.trim().isEmpty()) {
+            throw new IllegalArgumentException("prefix is blank");
+        }
+
+        this.temporaryDirectory = Files.createTempDirectory(prefix.trim() + UUID.randomUUID());
+    }
+
+    /**
+     * Get the temporary directory Path
+     *
+     * @return the temporary directory Path
+     */
+    public Path path() {
+        return temporaryDirectory;
+    }
+
+    /**
+     * Get the temporary directory Path
+     *
+     * @return the temporary directory Path
+     */
+    public Path getPath() {
+        return path();
+    }
+
+    @Override
+    public void close() throws IOException {
+        deleteRecursively(temporaryDirectory);
+    }
+
+    /**
+     * Method to recursively delete the temporary directory and all subdirectories/files
+     *
+     * @param path path
+     * @throws IOException IOException
+     */
+    private void deleteRecursively(Path path) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+}
---
+++ tests/src/test/java/org/verifyica/test/TemporaryDirectoryTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2024-present Verifyica project authors and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.verifyica.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Stream;
+import org.verifyica.api.ArgumentContext;
+import org.verifyica.api.TemporaryDirectory;
+import org.verifyica.api.Verifyica;
+
+public class TemporaryDirectoryTest {
+
+    private static final String TEMPORARY_DIRECTORY_DEFAULT_PREFIX = "temporary.directory.default.prefix";
+    private static final String TEMPORARY_DIRECTORY_CUSTOM_PREFIX = "temporary.directory.custom.prefix";
+
+    @Verifyica.ArgumentSupplier(parallelism = 4)
+    public static Object arguments() {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("test-1");
+        collection.add("test-2");
+        collection.add("test-3");
+        collection.add("test-4");
+        return collection;
+    }
+
+    @Verifyica.Test
+    public void createTemporaryDirectory1(ArgumentContext argumentContext) throws Throwable {
+        TemporaryDirectory temporaryDirectory = new TemporaryDirectory();
+        argumentContext.map().put(TEMPORARY_DIRECTORY_DEFAULT_PREFIX, temporaryDirectory);
+
+        System.out.printf(
+                "argument [%s] temporary directory [%s]%n",
+                argumentContext.testArgumentPayload(), temporaryDirectory.getPath());
+
+        assertThat(temporaryDirectory.path().toFile()).exists();
+        assertThat(temporaryDirectory.getPath().toFile()).exists();
+        assertThat(temporaryDirectory.path()).isSameAs(temporaryDirectory.getPath());
+    }
+
+    @Verifyica.Test
+    public void createTemporaryDirectory2(ArgumentContext argumentContext) throws Throwable {
+        TemporaryDirectory temporaryDirectory = new TemporaryDirectory("foo-");
+        argumentContext.map().put(TEMPORARY_DIRECTORY_CUSTOM_PREFIX, temporaryDirectory);
+
+        System.out.printf(
+                "argument [%s] temporary directory [%s]%n",
+                argumentContext.testArgumentPayload(), temporaryDirectory.getPath());
+
+        assertThat(temporaryDirectory.path().toFile()).exists();
+        assertThat(temporaryDirectory.getPath().toFile()).exists();
+        assertThat(temporaryDirectory.path()).isSameAs(temporaryDirectory.getPath());
+    }
+
+    @Verifyica.Conclude
+    public void conclude() throws Throwable {
+        // Assert that no temporary directories exist since they were scoped
+        // to the ArgumentContext which gets cleaned up after testing the argument
+        assertThat(noFileWithPrefixExists("temp-")).isTrue();
+        assertThat(noFileWithPrefixExists("foo-")).isTrue();
+    }
+
+    private static boolean noFileWithPrefixExists(String prefix) throws Throwable {
+        Path temporaryDirectory = Paths.get(System.getProperty("java.io.tmpdir"));
+        try (Stream<Path> files = Files.list(temporaryDirectory)) {
+            return files.noneMatch(file -> file.getFileName().toString().startsWith(prefix));
+        }
+    }
+}
