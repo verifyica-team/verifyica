fefd3fee48626a121e94153e2a72e1389cae4c90
antublue <antublue@antublue.org>
2024-10-23 09:03:15 -0400

    Fixed deadlock
    Signed-off-by: antublue <antublue@antublue.org>

--- engine/src/main/java/org/verifyica/engine/common/Streams.java
+++ engine/src/main/java/org/verifyica/engine/common/Streams.java
@@ -33,8 +33,8 @@ public class Streams {
     public static void fix() {
         synchronized (Streams.class) {
             if (!fixed) {
-                System.setErr(new SynchronizedPrintStream(System.err));
-                System.setOut(new SynchronizedPrintStream(System.out));
+                // System.setErr(new SynchronizedPrintStream(System.err));
+                // System.setOut(new SynchronizedPrintStream(System.out));
                 fixed = true;
             }
         }
