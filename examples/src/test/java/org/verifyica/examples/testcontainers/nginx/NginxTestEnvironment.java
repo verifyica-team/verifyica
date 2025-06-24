a37c4f7263e2dc9d9f4aff11ef7a9b9d4d7cae7a
antublue <antublue@antublue.org>
2025-06-24 09:47:52 -0400

    Updated Nginx test containers
    Signed-off-by: antublue <antublue@antublue.org>

--- examples/src/test/java/org/verifyica/examples/testcontainers/nginx/NginxTestEnvironment.java
+++ examples/src/test/java/org/verifyica/examples/testcontainers/nginx/NginxTestEnvironment.java
@@ -113,9 +113,7 @@ public class NginxTestEnvironment implements Argument<NginxTestEnvironment> {
      */
     public static Stream<NginxTestEnvironment> createTestEnvironments() {
         return Stream.of(
-                new NginxTestEnvironment("nginx:1.25.5"),
-                new NginxTestEnvironment("nginx:1.26.3"),
-                new NginxTestEnvironment("nginx:1.27.4"),
+                new NginxTestEnvironment("nginx:1.27.5"),
                 new NginxTestEnvironment("nginx:1.28.0"));
     }
 }
