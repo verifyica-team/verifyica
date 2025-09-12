1a7e7c33d56505b0db93a6742454af89149fed2d
antublue <antublue@antublue.org>
2025-09-12 15:22:49 -0400

    Added Bufstream example
    Signed-off-by: antublue <antublue@antublue.org>

---
+++ examples/src/test/java/org/verifyica/examples/testcontainers/bufstream/BufstreamTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) Verifyica project authors and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.verifyica.examples.testcontainers.bufstream;
+
+import static java.util.Optional.ofNullable;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.verifyica.examples.support.RandomSupport.randomString;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.testcontainers.containers.Network;
+import org.verifyica.api.Trap;
+import org.verifyica.api.Verifyica;
+import org.verifyica.examples.support.Logger;
+
+@SuppressWarnings("PMD.AvoidBranchingStatementAsLastInLoop")
+// @Verifyica.Disabled
+public class BufstreamTest {
+
+    private static final Logger LOGGER = Logger.createLogger(BufstreamTest.class);
+
+    private static final String TOPIC = "test";
+    private static final String GROUP_ID = "test-group-id";
+    private static final String EARLIEST = "earliest";
+
+    private final ThreadLocal<Network> networkThreadLocal = new ThreadLocal<>();
+    private final ThreadLocal<String> messageThreadLocal = new ThreadLocal<>();
+
+    @Verifyica.ArgumentSupplier(parallelism = Integer.MAX_VALUE)
+    public static Stream<BufstreamTestEnvironment> arguments() {
+        return BufstreamTestEnvironment.createTestEnvironments();
+    }
+
+    @Verifyica.BeforeAll
+    public void initializeTestEnvironment(BufstreamTestEnvironment bufstreamTestEnvironment) {
+        LOGGER.info("[%s] initialize test environment ...", bufstreamTestEnvironment.name());
+
+        Network network = Network.newNetwork();
+        network.getId();
+
+        networkThreadLocal.set(network);
+        bufstreamTestEnvironment.initialize(network);
+
+        assertThat(bufstreamTestEnvironment.isRunning()).isTrue();
+
+        LOGGER.info("bootstrap servers: %s", bufstreamTestEnvironment.bootstrapServers());
+    }
+
+    @Verifyica.Test
+    @Verifyica.Order(1)
+    public void testProduce(BufstreamTestEnvironment bufstreamTestEnvironment)
+            throws ExecutionException, InterruptedException {
+        LOGGER.info("[%s] testing testProduce() ...", bufstreamTestEnvironment.name());
+
+        String message = randomString(16);
+        LOGGER.info("[%s] producing message [%s] ...", bufstreamTestEnvironment.name(), message);
+
+        messageThreadLocal.set(message);
+
+        try (KafkaProducer<String, String> producer = createKafkaProducer(bufstreamTestEnvironment)) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(TOPIC, message);
+            producer.send(producerRecord).get();
+        }
+
+        LOGGER.info("[%s] message [%s] produced", bufstreamTestEnvironment.name(), message);
+    }
+
+    @Verifyica.Test
+    @Verifyica.Order(2)
+    public void testConsume1(BufstreamTestEnvironment bufstreamTestEnvironment) {
+        LOGGER.info("[%s] testing testConsume1() ...", bufstreamTestEnvironment.name());
+        LOGGER.info("[%s] consuming message ...", bufstreamTestEnvironment.name());
+
+        String expectedMessage = messageThreadLocal.get();
+        boolean messageMatched = false;
+
+        try (KafkaConsumer<String, String> consumer = createKafkaConsumer(bufstreamTestEnvironment)) {
+            consumer.subscribe(Collections.singletonList(TOPIC));
+
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(10000));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                LOGGER.info("[%s] consumed message [%s]", bufstreamTestEnvironment.name(), consumerRecord.value());
+                assertThat(consumerRecord.value()).isEqualTo(expectedMessage);
+                messageMatched = true;
+            }
+        }
+
+        assertThat(messageMatched).isTrue();
+    }
+
+    @Verifyica.Test
+    @Verifyica.Order(3)
+    public void testConsume2(BufstreamTestEnvironment bufstreamTestEnvironment) {
+        LOGGER.info("[%s] testing testConsume2() ...", bufstreamTestEnvironment.name());
+        LOGGER.info("[%s] consuming message ...", bufstreamTestEnvironment.name());
+
+        String expectedMessage = messageThreadLocal.get();
+        boolean messageMatched = false;
+
+        try (KafkaConsumer<String, String> consumer = createKafkaConsumer(bufstreamTestEnvironment)) {
+            consumer.subscribe(Collections.singletonList(TOPIC));
+
+            ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofMillis(5000));
+            for (ConsumerRecord<String, String> consumerRecord : consumerRecords) {
+                LOGGER.info("[%s] consumed message [%s]", bufstreamTestEnvironment.name(), consumerRecord.value());
+                assertThat(consumerRecord.value()).isEqualTo(expectedMessage);
+                messageMatched = true;
+            }
+        }
+
+        assertThat(messageMatched).isFalse();
+    }
+
+    @Verifyica.AfterAll
+    public void destroyTestEnvironment(BufstreamTestEnvironment bufstreamTestEnvironment) throws Throwable {
+        LOGGER.info("[%s] destroy test environment ...", bufstreamTestEnvironment.name());
+
+        List<Trap> traps = new ArrayList<>();
+
+        traps.add(new Trap(bufstreamTestEnvironment::destroy));
+        traps.add(new Trap(() -> ofNullable(networkThreadLocal.get()).ifPresent(Network::close)));
+        traps.add(new Trap(messageThreadLocal::remove));
+        traps.add(new Trap(networkThreadLocal::remove));
+
+        Trap.assertEmpty(traps);
+    }
+
+    /**
+     * Method to create a KafkaProducer
+     *
+     * @param bufstreamTestEnvironment bufstreamTestEnvironment
+     * @return a KafkaProducer
+     */
+    private static KafkaProducer<String, String> createKafkaProducer(
+            BufstreamTestEnvironment bufstreamTestEnvironment) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bufstreamTestEnvironment.bootstrapServers());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(properties);
+    }
+
+    /**
+     * Method to create a KafkaConsumer
+     *
+     * @param bufstreamTestEnvironment bufstreamTestEnvironment
+     * @return a KafkaConsumer
+     */
+    private static KafkaConsumer<String, String> createKafkaConsumer(
+            BufstreamTestEnvironment bufstreamTestEnvironment) {
+        Properties properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bufstreamTestEnvironment.bootstrapServers());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, EARLIEST);
+
+        return new KafkaConsumer<>(properties);
+    }
+}
---
+++ examples/src/test/java/org/verifyica/examples/testcontainers/bufstream/BufstreamTestEnvironment.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) Verifyica project authors and contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.verifyica.examples.testcontainers.bufstream;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Ports;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Objects;
+import java.util.stream.Stream;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.Transferable;
+import org.testcontainers.utility.DockerImageName;
+import org.verifyica.api.Argument;
+import org.verifyica.examples.support.TextBlock;
+
+/**
+ * Class to implement a BufstreamTestEnvironment
+ */
+public class BufstreamTestEnvironment implements Argument<BufstreamTestEnvironment> {
+
+    private final String dockerImageName;
+    private GenericContainer<?> genericContainer;
+
+    /**
+     * Constructor
+     *
+     * @param dockerImageName the name
+     */
+    public BufstreamTestEnvironment(String dockerImageName) {
+        this.dockerImageName = dockerImageName;
+    }
+
+    /**
+     * Method to get the name
+     *
+     * @return the name
+     */
+    @Override
+    public String getName() {
+        return dockerImageName;
+    }
+
+    /**
+     * Method to get the payload
+     *
+     * @return the payload
+     */
+    @Override
+    public BufstreamTestEnvironment getPayload() {
+        return this;
+    }
+
+    /**
+     * Method to initialize the BufstreamTestEnvironment using a specific network
+     *
+     * @param network the network
+     */
+    public void initialize(Network network) {
+        // info("initialize test environment [%s] ...", dockerImageName);
+
+        for (int i = 0; i < 10; i++) {
+            int HOST_KAFKA = getFreePort();
+            int HOST_ADMIN = getFreePort();
+
+            String configuration = new TextBlock()
+                    .line("kafka:")
+                    .line("  address: 0.0.0.0:9092")
+                    .line("  public_address: localhost:" + HOST_KAFKA)
+                    .line("  num_partitions: 1")
+                    .line("admin_address: 0.0.0.0:9089")
+                    .toString();
+
+            try {
+                genericContainer = new GenericContainer<>(DockerImageName.parse(dockerImageName))
+                        .withNetwork(network)
+                        .withNetworkAliases("bufstream")
+                        .withExposedPorts(9092, 9089)
+                        .withCopyToContainer(Transferable.of(configuration), "/etc/bufstream.yaml")
+                        .withCommand("serve", "--inmemory", "-c", "/etc/bufstream.yaml")
+                        // bind fixed host ports -> container ports
+                        .withCreateContainerCmdModifier(createContainerCmd -> {
+                            Ports bindings = new Ports();
+                            bindings.bind(ExposedPort.tcp(9092), Ports.Binding.bindPort(HOST_KAFKA));
+                            bindings.bind(ExposedPort.tcp(9089), Ports.Binding.bindPort(HOST_ADMIN));
+                            Objects.requireNonNull(createContainerCmd.getHostConfig())
+                                    .withPortBindings(bindings);
+                        })
+                        .waitingFor(Wait.forHttp("/-/status").forPort(9089).forStatusCode(200));
+
+                genericContainer.start();
+
+                break;
+            } catch (Exception e) {
+                genericContainer.stop();
+
+                if (i < 9) {
+                    // try again
+                    continue;
+                }
+
+                throw e;
+            }
+        }
+
+        // info("test environment [%s] initialized", dockerImageName);
+    }
+
+    /**
+     * Method to determine if the BufstreamTestEnvironment is running
+     *
+     * @return true if the BufstreamTestEnvironment is running, otherwise false
+     */
+    public boolean isRunning() {
+        return genericContainer.isRunning();
+    }
+
+    /**
+     * Method to get the Bufstream bootstrap servers
+     *
+     * @return the Bufstream bootstrap servers
+     */
+    public String bootstrapServers() {
+        return genericContainer.getHost() + ":" + genericContainer.getMappedPort(9092);
+    }
+
+    /**
+     * Method to destroy the BufstreamTestEnvironment
+     */
+    public void destroy() {
+        // info("destroying test environment [%s] ...", dockerImageName);
+
+        if (genericContainer != null) {
+            genericContainer.stop();
+            genericContainer = null;
+        }
+
+        // info("test environment [%s] destroyed", dockerImageName);
+    }
+
+    /**
+     * Method to create a Stream of BufstreamTestEnvironment
+     *
+     * @return a Stream of BufstreamTestEnvironment
+     */
+    public static Stream<BufstreamTestEnvironment> createTestEnvironments() {
+        return Stream.of(new BufstreamTestEnvironment("bufbuild/bufstream:0.3.44"));
+    }
+
+    /**
+     * Method to get a free port on localhost
+     *
+     * @return a free port on localhost
+     */
+    private static int getFreePort() {
+        try (ServerSocket serverSocket = new ServerSocket(0)) {
+            serverSocket.setReuseAddress(true);
+            return serverSocket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
